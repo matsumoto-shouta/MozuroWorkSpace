3101016b41b6f22c15caabb14b412076472b7b34
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-05-17 10:48:56 +0900

    db_connect作成

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SARVER = 'mysql305.phy.lolipop.lan';
+    const DBNAME = 'LAA1517807-insta';
+    const USER = 'LAA1517807';
+    const PASS = 'Pass0514';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
