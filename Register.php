7a8fdabcd9d099231a40a91a510e32d4f051676a
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-05-17 12:09:19 +0900

    ユーザー登録・更新(機能のみ)

---
+++ Register.php
@@ -0,0 +1,55 @@
+<?php session_start(); ?>
+<?php require 'DB-connect.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザー登録</title>
+</head>
+<body>
+    
+    <?php
+        $pdo = new PDO($connect, USER, PASS);
+        if(isset($_SESSION['UserDate'])){
+            $id = $_SESSION['UserDate']['user_ID'];
+            $sql = $pdo->prepare('select * from UserDate where user_ID = ? and mail = ?');
+            $sql->execute([$id, $_POST['mail']]);
+        }else{
+            $sql = $pdo->prepare('select * from UserDate where mail =?');
+            $sql->execute([$_POST['mail']]);
+        }
+
+        if(empty($sql->fetchAll())){
+            if(isset($_SESSION['UserData'])){
+                $sql = $pdo->prepare('insert into UserDate(user_ID, user_name, mail, pass) values(?,?,?,?)');
+                $sql->execute([$id,
+                                $_POST['user_name'],
+                                $_POST['mail'],
+                                $_POST['pass']
+                            ]);
+
+                $_SESSION['UserData'] = [
+                    'user_ID' => $id,
+                    'user_name' => $_POST['user_name'],
+                   'mail' => $_POST['mail'],
+                ];
+                
+                echo 'ユーザー情報を更新しました';
+            }else{
+                $sql = $pdo->prepare('insert into UserDate(user_name, mail, pass) values(?,?,?)');
+                $sql->execute([
+                    $_POST['user_name'],
+                    $_POST['mail'],
+                    $_POST['pass']
+                ]);
+
+                echo 'ユーザー情報を登録しました';
+            }
+        }else{
+            echo 'メールアドレスが重複しています';
+        }
+        ?>
+
+</body>
+</html>
