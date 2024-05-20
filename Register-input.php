3a196991d4edd2377e69540d9467fbf32dee8383
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-05-20 12:29:54 +0900

    変更

--- Register-input.php
+++ Register-input.php
@@ -2,14 +2,14 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>ユーザー登録(入力)</title>
 </head>
 <body>
 
     <h1>ユーザー情報の登録</h1>
 
-    <from action="Register-output.php" method="post">
+    <form action="Register-output.php" method="post">
 
     <p>ユーザーネーム</p>
     <input type="label" name="user_name">
@@ -19,7 +19,7 @@
     <input type="password" name="pass">
 
     <button type="submit">登録</button>
-    </from>
+    </form>
 
 </body>
 </html>
