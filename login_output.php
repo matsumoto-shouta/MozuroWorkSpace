cd053172c3d6e2b6d1ec72ed6147fa635936964e
yuma-ness <yuma0514ness@icloud.com>
2024-05-22 11:48:02 +0900

    a

---
+++ login_output.php
@@ -0,0 +1,38 @@
+<?php session_start(); ?>
+<?php require 'header.php'; ?>
+<?php require 'db-connect.php'; ?>
+<link rel="stylesheet" href="css/login_output.css?v=1.0.1">
+<div class="flex">
+<figure class="image"><img 
+src="image/rogo.jpg">
+</figure>
+<h1>ログイン<h1>
+</div>
+<?php 
+unset($_SESSION['UserData']);
+if($_POST['password'] != null && $_POST['login'] != null){
+$pdo = new PDO($connect,USER,PASS);
+$sql = $pdo->prepare('select * from UserData where name=?');
+$sql->execute([$_POST['login']]);
+foreach($sql as $row){
+    if($_POST["password"]==$row['password']){
+    $_SESSION['UserData']=[
+        // rowの中身にsql（DB）の内容が入ってる。rowの中身をセッションのそれぞれの所に入れてる。
+        'id'=>$row['id'],'name'=>$row['name'],'mail'=>$row['mail'],
+        'pass'=>$row['pass']];
+    }
+}
+if(isset($_SESSION['UserData'])){
+    echo '<p class="log">いらっしゃいませ、',$_SESSION['UserData']['name'],'さん。</p>';
+//    echo '<a href="shohin_top.php" id="my"><button>商品一覧へ</button></a>';
+
+}else{
+    echo '<p class="log">ログイン名またはパスワードが違います。</p>';
+    echo '<a href="login_input.php" id="my"><button>ログイン画面へ</button></a>';
+}
+}else{
+    echo '<p class="log">ログイン名またはパスワードを入力してください。</p>';
+    echo '<a href="login_input.php" id="my"><button>ログイン画面へ</button></a>';
+}
+?>
+<?php require 'footer.php'; ?>
