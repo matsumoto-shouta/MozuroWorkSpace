5c731f7c66bc2d70cfd9f16ac726b72439a7697e
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-05-22 12:03:20 +0900

    change

--- Posting-input.php
+++ Posting-input.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
+<?php require 'DB-connect.php'; ?>
 <?php $csrf = base64_encode( openssl_random_pseudo_bytes( 32 ) ); ?>
 <?php $_SESSION['csrf'] = $csrf; ?>
-<?php require 'DB-connect.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
