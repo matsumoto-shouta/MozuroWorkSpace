4ebe749b11e9223a35667e0eb56b8354d3c29891
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-05-28 12:43:44 +0900

    css更新

--- Register-input.php
+++ Register-input.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-
+    <link rel="stylesheet" href="./css/Register.css">
     <title>ユーザー登録(入力)</title>
 </head>
 <body>
@@ -11,13 +11,11 @@
 
     <form action="Register-output.php" method="post">
 
-    <p>ユーザーネーム</p>
-    <input type="label" name="user_name">
-    <p>メールアドレス</p>
-    <input type="label" name="mail">
-    <p>パスワード</p>
-    <input type="password" name="pass">
-
+    <div class="form-group">
+        <input type="label" name="user_name" placeholder="ユーザーネーム">
+        <input type="label" name="mail" placeholder="メールアドレス">
+        <input type="password" name="pass" placeholder="パスワード">
+    </div>
     <button type="submit">登録</button>
     </form>
 
