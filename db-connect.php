3ac9126f36c291a45c75c9b53de0c4db0bd0832e
yuma-ness <yuma0514ness@icloud.com>
2024-06-03 11:47:48 +0900

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -3,7 +3,6 @@
     const DBNAME = 'LAA1517807-insta';
     const USER = 'LAA1517807';
     const PASS = 'Pass0514';
-
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
     $pdo = new PDO($connect,USER,PASS);
 ?>
