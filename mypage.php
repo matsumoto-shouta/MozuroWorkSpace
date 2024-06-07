5645fb769ef689a453c2775379fc9e49fdcfb977
watanabe-kenta-web <2201232@s.asojuku.ac.jp>
2024-06-07 11:39:26 +0900

    a

--- mypage.php
+++ mypage.php
@@ -4,11 +4,11 @@
     <title>マイページ</title>
 </head>
 <body>
-<img src="image/kkrn_icon_user_13.svg"><br>
+    <img src="image/kkrn_icon_user_13.svg"><br>
 
-<form action="profileedit-input" method="post">
-    <button type="submit">プロフィールを編集</button>
-</from><br>
+    <form action="profileedit-input" method="post">
+        <button type="submit">プロフィールを編集</button>
+    </form><br>
 
     <form action="home.php" method="post">
         <button type="submit">ホームに戻る</button>
