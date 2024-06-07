01ed031e5c187d1dd721de4e980d0605e13eacb1
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-06-07 12:45:21 +0900

    Login.cssほぼ完成

--- login_input.php
+++ login_input.php
@@ -1,11 +1,14 @@
 <div class="all">
 <link rel="stylesheet" href="css/Login.css">
 <img class="main-image" src="image/wakadan.jpg">
-<form class="form-group" action="login_output.php" method="post">
-    <input type="text"  class="content" name="login" placeholder="ユーザーネーム">
-    <input type="password" class="content" name="password" placeholder="パスワード">
+<form action="login_output.php" method="post">
+    <div class="form-group">
+        <input type="text"  class="content" name="login" placeholder="ユーザーネーム">
+        <input type="password" class="content" name="password" placeholder="パスワード">
+    </div>
     <button class="btn" type="submit">ログイン</button>
 </form>
-<p>初めての方はこちらから<br>↓</p>
-<a href="Register-input.php"><button>新規登録</button></a>
+<form action="Register-input.php" method="post">
+    <button class="btn2" type="submit">初めての方はこちら</button>
+</form>
 </div> 
