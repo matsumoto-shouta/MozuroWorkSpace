c72e12bbebfe40768c4ad8eb5c5a02175805f2bd
matsumoto-shouta <148170851+matsumoto-shouta@users.noreply.github.com>
2024-06-14 12:45:42 +0900

    indexcss

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
     <h2>画像アップロードフォーム</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label for="file">画像を選択してください:</label>
+        <label for="file" class="select">画像を選択してください:</label><br>
         キャプション: <input type="text" name="caption" id="caption"><br>
         <input type="file" name="file" id="file">
         <input type="submit" value="アップロード">
