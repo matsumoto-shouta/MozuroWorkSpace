94140061f14ccc6cd4bf32e9023687e1cdb3bce4
KoyamaIssei <2201145@s.asojuku.ac.jp>
2024-06-17 10:16:26 +0900

    ログアウトCSS

--- logout_input.php
+++ logout_input.php
@@ -1,14 +1,15 @@
-<link rel="stylesheet" href="css/logout-input.css">
-<div class="flex">
-<figure class="image"><img 
-src="image/rogo.jpg">
-</figure>
-<h1>ログアウト<h1>
-</div>
-<div id='a'>
+<div class="all">
+<link rel="stylesheet" href="./css/Logout.css">
+    <div class="flex">
+            <figure class="image"><img src="image/instakiro.png" width="100" height="100">
+            </figure>
+        <h1>ログアウト<h1>
+    </div>
+    <div id='a'>
 
-<p class='log'>ログアウトします本当によろしいですか？</br>
-ログアウト後のログイン画面へ移行します</p>
-</div>
-<a href="logout_output.php"><button>ログアウト</button></a>
+        <p class='log'>ログアウトします本当によろしいですか？</br>
+        ログアウト後のログイン画面へ移行します</p>
+    </div>
+<a href="logout_output.php"><button class="btn">ログアウト</button></a>
 <!-- <a href="mypage.php" id="my"><button>マイページへ</button></a> -->
+</div>
