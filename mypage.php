ef0fbb95e6879c7a11502cc8d454d8b02b5e701f
watanabe-kenta-web <2201232@s.asojuku.ac.jp>
2024-06-19 12:23:12 +0900

    a

--- mypage.php
+++ mypage.php
@@ -14,33 +14,37 @@
         <button type="submit">プロフィールを編集</button>
     </form><br>
 
-    <!-- <img src="uploads/uploaded_image.png" alt="アップロードされた画像"><br> -->
     <div class="container">
-    <div class="gallery">
-        <?php
-        if(isset($_SESSION['UserData']['id'])){
-            $sql = "SELECT * FROM Upload 
-                    JOIN Picture ON Upload.picture_ID = Picture.picture_ID 
-                    JOIN UserData ON UserData.user_ID = Upload.user_ID";
-            $stmt = $pdo->query($sql);
+        <div class="gallery">
+            <?php
+            if(isset($_SESSION['UserData']['id'])){
+                $user_id = $_SESSION['UserData']['id'];
+                $sql = "SELECT Upload.*, Picture.picture_name, UserData.user_name 
+                        FROM Upload 
+                        JOIN Picture ON Upload.picture_ID = Picture.picture_ID 
+                        JOIN UserData ON UserData.user_ID = Upload.user_ID 
+                        WHERE Upload.user_ID = :user_id";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+                $stmt->execute();
 
-            if ($stmt->rowCount() > 0) {
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<div class='gallery-item'>";
-                    echo "<a href='image.php?id=" . htmlspecialchars($row['picture_ID']) . "'>";
-                    echo "<img src='" . htmlspecialchars($row['picture_name']) . "' alt='アップロードされた画像'>";
-                    echo "<div class='overlay'>";
-                    echo "<div class='text'>" . htmlspecialchars($row['user_name']) . "</div>";
-                    echo "<div class='text'>" . htmlspecialchars($row['caption']) . "</div>";
-                    echo "</div>";
-                    echo "</a>";
-                    echo "</div>";
+                if ($stmt->rowCount() > 0) {
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<div class='gallery-item'>";
+                        echo "<a href='image.php?id=" . htmlspecialchars($row['picture_ID']) . "'>";
+                        echo "<img src='" . htmlspecialchars($row['picture_name']) . "' alt='アップロードされた画像'>";
+                        echo "<div class='overlay'>";
+                        echo "<div class='text'>" . htmlspecialchars($row['user_name']) . "</div>";
+                        echo "<div class='text'>" . htmlspecialchars($row['caption']) . "</div>";
+                        echo "</div>";
+                        echo "</a>";
+                        echo "</div>";
+                    }
                 }
             }
-        }
-        ?>
+            ?>
+        </div>
     </div>
-</div>
     
     <form action="home.php" method="post">
         <button type="submit">ホームに戻る</button>
