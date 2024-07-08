ecd6498572b6f7caa8f5542e6b26363cd543f674
yuma-ness <yuma0514ness@icloud.com>
2024-07-08 09:32:47 +0900

    asd

--- home.php
+++ home.php
@@ -28,7 +28,7 @@
                     echo "<a href='image.php?id=" . htmlspecialchars($row['picture_ID']) . "'>";
                     echo "<img src='" . htmlspecialchars($row['picture_name']) . "' alt='アップロードされた画像'>";
                     echo "<div class='overlay'>";
-                   // echo "<div class='text'>" . htmlspecialchars($row['user_name']) . "</div>";
+                    echo "<div class='text'>" . htmlspecialchars($row['user_name']) . "</div>";
                     echo "<div class='text'>" . htmlspecialchars($row['caption']) . "</div>";
                     echo "</div>";
                     echo "</a>";
@@ -73,62 +73,5 @@
     .like-count {
         font-size: 18px;
         margin-left: 10px;
-    }<?php session_start(); ?>
-<link rel="stylesheet" href="css/home.css?v=1.0.1">
-
-
-<?php require 'db-connect.php'; ?>  
-<div class="container">
-    <?php require "hamburger.php"; ?>
-    
-    <h2>画像ギャラリー</h2>
-    <div class="gallery">
-        <?php
-        if(isset($_SESSION['UserData']['id'])){
-            $user_ID = $_SESSION['UserData']['id'];
-            $sql = "SELECT Picture.picture_ID, Picture.picture_name, UserData.user_name, UserData.user_picture, Upload.caption, COUNT(likes.id) as like_count,
-                    (SELECT COUNT(*) FROM likes WHERE likes.post_id = Picture.picture_ID AND likes.user_ID = :user_ID) as user_liked
-                    FROM Upload 
-                    JOIN Picture ON Upload.picture_ID = Picture.picture_ID 
-                    JOIN UserData ON UserData.user_ID = Upload.user_ID
-                    LEFT JOIN likes ON Picture.picture_ID = likes.post_id
-                    GROUP BY Picture.picture_ID";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute(['user_ID' => $user_ID]);
-
-            if ($stmt->rowCount() > 0) {
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $likedClass = $row['user_liked'] > 0 ? 'liked' : '';
-                    echo "<div class='gallery-item'>";
-                    echo "<a href='image.php?id=" . htmlspecialchars($row['picture_ID']) . "'>";
-                    echo "<div class='overlay'>";
-                    echo "<div class='user-info'>";
-                    echo "<img src='" . htmlspecialchars($row['user_picture']) . "' alt='ユーザーアイコン' class='user-icon'>";
-                    echo "<span class='user-name'>" . htmlspecialchars($row['user_name']) . "</span>";
-                    echo "</div>";
-                    echo "<img src='" . htmlspecialchars($row['picture_name']) . "' alt='アップロードされた画像'>";
-                    echo "<div class='text'>" . htmlspecialchars($row['caption']) . "</div>";
-                    echo "</div>";
-                    echo "</a>";
-                    echo "<form action='like.php' class='likeBn' method='post'>";
-                    echo "<input type='hidden' name='post_ID' value='" . htmlspecialchars($row['picture_ID']) . "'>";
-                    echo "<button type='submit' class='like-button $likedClass'>";
-                    echo "<i class='fa fa-heart'></i>";
-                    echo "</button>";
-                    echo "<span class='like-count'> " . htmlspecialchars($row['like_count']) . "</span>";
-                    echo "</form>";
-                    echo "</div>";
-                }
-            } else {
-                echo "<p>ギャラリーに画像がありません。</p>";
-            }
-        } else {
-            echo "<p>ログインしてください</p>";
-        }
-        ?>
-    </div>
-</div>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    }
 </style>
