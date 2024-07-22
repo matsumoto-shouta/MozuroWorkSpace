0e6dcb847574127cb813bf6fec46f3f65721103b
KoyamaIssei <2201145@s.asojuku.ac.jp>
2024-07-22 11:45:07 +0900

    修正

--- Register-input.php
+++ Register-input.php
@@ -6,17 +6,18 @@
     <title>ユーザー登録(入力)</title>
 </head>
 <body>
-
-    <h1>ユーザー情報の登録</h1>
 <div class="all">
+    <h1>ユーザー情報の登録</h1>
     <form action="Register-output.php" method="post">
-
-    <div class="form-group">
+        <div class="form-group">
             <input type="text" class="content" name="user_name" placeholder="ユーザーネーム">
             <input type="text" class="content" name="mail" placeholder="メールアドレス">
             <input type="password" class="content" name="pass" placeholder="パスワード">
-    </div>
-    <button class="btn" type="submit">登録</button>
+        </div>
+        <div class="group">
+            <button class="btn" type="button" onclick="window.location.href='login_input.php'">戻る</button>
+            <button class="btn" type="submit">登録</button>
+        </div>
     </form>
 </div>
 </body>
